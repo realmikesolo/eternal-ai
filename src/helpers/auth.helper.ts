4df42e741f1d74454b7f54542e8e15f3452d7472
realmikesolo <aeryoff@gmail.com>
2023-05-27 21:54:46 +0200

    fix: auth helper

--- src/helpers/auth.helper.ts
+++ src/helpers/auth.helper.ts
@@ -3,5 +3,5 @@ import { Env } from '../shared/env';
 import { User } from '../models/user.model';
 
 export function generateToken(user: User): string {
-  return sign(user, Env.JWT_SECRET, { expiresIn: '1d' });
+  return sign(JSON.parse(JSON.stringify(user)), Env.JWT_SECRET, { expiresIn: '1d' });
 }
