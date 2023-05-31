bca18e1dffa964fb66b7149e18f6909bbe87232f
realmikesolo <aeryoff@gmail.com>
2023-05-31 11:48:13 +0200

    fix: deploy

--- src/migrations/1685459589227-init.ts
+++ src/migrations/1685459589227-init.ts
@@ -1,14 +1,13 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Init1685459589227 implements MigrationInterface {
-    name = 'Init1685459589227'
+  name = 'Init1685459589227';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" ADD "phoneNumber" character varying(255)`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "users" ADD "phoneNumber" character varying(255)`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "phoneNumber"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "phoneNumber"`);
+  }
 }
