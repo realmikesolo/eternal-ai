516aceae5fb9864f1fc436488d61f7bb70e0e65a
realmikesolo <aeryoff@gmail.com>
2023-06-01 17:25:02 +0200

    fix: payment router

--- src/routes/payment.router.ts
+++ src/routes/payment.router.ts
@@ -6,49 +6,49 @@ import Stripe from 'stripe';
 const paymentService = new PaymentService();
 
 export async function paymentRouter(fastify: FastifyInstance): Promise<void> {
+  fastify.post('/subsrcibe', {}, async (req, res) => {
+    const subscription = await paymentService.subscribe(req.body);
+    res.status(200).send(subscription);
+  });
+
   fastify.post(
-    '/subsrcibe',
+    '/webhook',
     {
       config: {
         rawBody: true,
       },
     },
     async (req, res) => {
-      const subscription = await paymentService.subscribe(req.body);
-      res.status(200).send(subscription);
+      const endpointSecret = 'we_1NECjwInsB9f0TAdRIIxRSH3';
+
+      const sig = req.headers['stripe-signature']!;
+
+      let event: Stripe.Event;
+
+      try {
+        event = stripeClient.webhooks.constructEvent(req.body as string | Buffer, sig, endpointSecret);
+      } catch (e) {
+        console.log(e);
+        throw e;
+      }
+      console.log(300, event);
+      // Handle the event
+      switch (event.type) {
+        case 'customer.subscription.created':
+          // eslint-disable-next-line no-case-declarations
+          const customerSubscriptionCreated = event.data.object;
+          console.log(500, customerSubscriptionCreated);
+          // Then define and call a function to handle the event customer.subscription.created
+          break;
+        case 'customer.subscription.deleted':
+          // eslint-disable-next-line no-case-declarations
+          const customerSubscriptionDeleted = event.data.object;
+          console.log(600, customerSubscriptionDeleted);
+          // Then define and call a function to handle the event customer.subscription.deleted
+          break;
+        default:
+          console.log(`Unhandled event type ${event.type}`);
+      }
     },
   );
-
-  fastify.post('/webhook', {}, async (req, res) => {
-    const endpointSecret = 'we_1NECjwInsB9f0TAdRIIxRSH3';
-
-    const sig = req.headers['stripe-signature']!;
-
-    let event: Stripe.Event;
-
-    try {
-      event = stripeClient.webhooks.constructEvent(req.body as string | Buffer, sig, endpointSecret);
-    } catch (e) {
-      console.log(e);
-      throw e;
-    }
-    console.log(300, event);
-    // Handle the event
-    switch (event.type) {
-      case 'customer.subscription.created':
-        // eslint-disable-next-line no-case-declarations
-        const customerSubscriptionCreated = event.data.object;
-        console.log(500, customerSubscriptionCreated);
-        // Then define and call a function to handle the event customer.subscription.created
-        break;
-      case 'customer.subscription.deleted':
-        // eslint-disable-next-line no-case-declarations
-        const customerSubscriptionDeleted = event.data.object;
-        console.log(600, customerSubscriptionDeleted);
-        // Then define and call a function to handle the event customer.subscription.deleted
-        break;
-      default:
-        console.log(`Unhandled event type ${event.type}`);
-    }
-  });
 }
